a2e27c1bb023e41ef783ea23400a458b5189e1b5
CunningBlade <om.raj.singh.dev@gmail.com>
2026-08-14 21:52:41 +0530

    Time: 0 ms (100%), Space: 10.8 MB (6.13%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,18 +1,14 @@
 class Solution {
 public:
-    int maxLoot(int idx, vector<int>& nums, vector<int>& dp){
-        if(idx == 0) return nums[idx];
-        if(idx < 0) return 0;
-        if(dp[idx] != -1) return dp[idx];
-
-        int pick = nums[idx] + maxLoot(idx-2, nums, dp);
-        int not_pick = 0 + maxLoot(idx-1, nums, dp);
-
-        dp[idx] = max(pick, not_pick);
-        return dp[idx]; 
-    }
     int rob(vector<int>& nums) {
-        vector<int>dp(nums.size(), -1);
-        return maxLoot(nums.size()-1, nums, dp);
+        vector<int>dp(nums.size(), 0);
+        dp[0] = nums[0];
+        for(int i = 1; i < nums.size() ; i++){
+            int take = nums[i];
+            if(i > 1) take += dp[i-2];
+            int not_take = 0 + dp[i-1];
+            dp[i] = max(take, not_take);
+        }
+        return dp[nums.size()-1];
     }
 };
